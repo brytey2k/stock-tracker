a7040d78953747fd39a9567035b1e54d0e2047d4
Bright Nkrumah <brytey2k@gmail.com>
2023-03-04 22:34:26 +0000

    Chore: updates setup script with JWT setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 composer install
 ./vendor/bin/sail up -d
 ./vendor/bin/sail artisan key:generate
+./vendor/bin/sail artisan vendor:publish --provider="PHPOpenSourceSaver\JWTAuth\Providers\LaravelServiceProvider"
+./vendor/bin/sail artisan jwt:secret
 ./vendor/bin/sail artisan migrate --seed
